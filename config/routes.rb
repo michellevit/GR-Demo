9c1982f064114b16856794219922b849d4b51081
michellevit <mvflandin@gmail.com>
2024-02-09 15:57:34 -0800

    Modify route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :products do
-    get 'new', to: 'products#new', as: 'new_product'
-  end
+  resources :products
   root 'home#index'
    # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
