fa6156d6fc2d65ece6665148f0fa8a8be30698eb
michellevit <mvflandin@gmail.com>
2024-02-09 13:14:36 -0800

    Prep migration

--- db/migrate/20240209204620_change_image_field.rb
+++ db/migrate/20240209204620_change_image_field.rb
@@ -1,5 +1,15 @@
 class ChangeImageField < ActiveRecord::Migration[7.1]
   def change
-    change_column :products, :image_urls, 'jsonb USING CAST(image_urls AS jsonb)'
+    reversible do |dir|
+      change_table :products do |t|
+        dir.up do
+          t.change :image_urls, 'jsonb USING CAST(image_urls AS jsonb)'
+        end
+
+        dir.down do
+          t.change :image_urls, 'text USING CAST(image_urls AS text)'
+        end
+      end
+    end
   end
 end
