0ab3a7c6f879fcc2349ada164517ab9e5d3e6dfe
michellevit <mvflandin@gmail.com>
2024-02-09 15:33:57 -0800

    Modify products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,7 +8,7 @@ class ProductsController < ApplicationController
 
   # GET /products/1 or /products/1.json
   def show
-    product = Product.find(params[:id])
+    @product = Product.find(params[:id])
     render json: @products, status: :ok
   rescue ActiveRecord::RecordNotFound
     render json: { error: "Product not found" }, status: :not_found
