031f452b968a042d4b4848606c20bb53ffa04e08
michellevit <mvflandin@gmail.com>
2024-02-09 13:23:48 -0800

    Committing the db/migrate folder

--- db/migrate/20240209182116_change_image_urls_to_json.rb
+++ db/migrate/20240209182116_change_image_urls_to_json.rb
@@ -1,5 +1,5 @@
 class ChangeImageUrlsToJson < ActiveRecord::Migration[7.1]
   def change
-    change_column :products, :image_urls, :jsonb
+    change_column :products, :image_urls, 'jsonb USING CAST(image_urls AS jsonb)'
   end
 end
