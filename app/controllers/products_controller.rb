f50dd277d95e80bc03e28e073b9e6be7f4c80a1a
michellevit <mvflandin@gmail.com>
2024-02-09 15:05:55 -0800

    Modify products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,7 @@ class ProductsController < ApplicationController
   # GET /products or /products.json
   def index
     @products = Product.all
+    flash.now[:notice] = "Your notice message"
   end
 
   # GET /products/1 or /products/1.json
