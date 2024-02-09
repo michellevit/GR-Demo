284f8da86c5ebedc1a7a2d84dd947ee745067207
michellevit <mvflandin@gmail.com>
2024-02-09 15:02:50 -0800

    Modify products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,6 @@ class ProductsController < ApplicationController
   # GET /products or /products.json
   def index
     @products = Product.all
-    render json: @products, status: :ok
   end
 
   # GET /products/1 or /products/1.json
