cf55973fa1b282676d1405673b44a4911a1ab957
michellevit <mvflandin@gmail.com>
2024-02-10 13:31:31 -0800

    Test build 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   resources :products
   root 'application#react_app'
-  get '/backend', to: 'home#index', as: 'backend'
-  # Route for searching products
-  get '/products/search', to: 'products#search', as: 'search_products'
   # Health check route
   get '/up', to: 'rails/health#show', as: :rails_health_check
 end
