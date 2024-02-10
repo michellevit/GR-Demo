dc8b62106d3ef1b5b237301c5314d141fd29ce8d
michellevit <mvflandin@gmail.com>
2024-02-10 01:38:59 -0800

    Fix minor issue in AllProduct.js

--- reactapp/src/pages/AllProducts.js
+++ reactapp/src/pages/AllProducts.js
@@ -5,12 +5,12 @@ import ProductCard from "../components/ProductCard";
 
 const AllProducts = () => {
   const [products, setProducts] = useState([]);
-  
+  console.log(products);
   useEffect(() => {
     const fetchProducts = async () => {
       try {
         const response = await axios.get('/products');
-        console.log("Fetched products:", response.data); 
+        console.log("Fetched products:", response.data);
         setProducts(response.data); 
       } catch (error) {
         console.error("There was an error fetching the products: ", error);
@@ -18,37 +18,39 @@ const AllProducts = () => {
     };
 
     fetchProducts();
+    console.log(products);
+
   }, []);
 
   return (
-    <div className="all-products-container">
-      <div className="section-container">
-        <div className="paragraphs">
-          <h2>Recommended for you</h2>
-          <div className="recommended-section">
-            {Array.isArray(products) && products.map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </div>
-        <div className="paragraphs">
-          <h2>Liked</h2>
-          <div className="staff-picks-section">
-            {Array.isArray(products) && products.map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </div>
-        <div className="paragraphs">
-          <h2>Staff Picks</h2>
-          <div className="liked-section">
-            {Array.isArray(products) && products.map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </div>
-      </div>
-    </div>
+    // <div className="all-products-container">
+    //   <div className="section-container">
+    //     <div className="paragraphs">
+    //       <h2>Recommended for you</h2>
+    //       <div className="recommended-section">
+    //         {Array.isArray(products) && products.map((product) => (
+    //           <ProductCard key={product.id} product={product} />
+    //         ))}
+    //       </div>
+    //     </div>
+    //     <div className="paragraphs">
+    //       <h2>Liked</h2>
+    //       <div className="staff-picks-section">
+    //         {Array.isArray(products) && products.map((product) => (
+    //           <ProductCard key={product.id} product={product} />
+    //         ))}
+    //       </div>
+    //     </div>
+    //     <div className="paragraphs">
+    //       <h2>Staff Picks</h2>
+    //       <div className="liked-section">
+    //         {Array.isArray(products) && products.map((product) => (
+    //           <ProductCard key={product.id} product={product} />
+    //         ))}
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
