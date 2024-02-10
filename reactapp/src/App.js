0e95ad1bc89dea6a7fd627db2edd892cf019df49
michellevit <mvflandin@gmail.com>
2024-02-10 13:43:54 -0800

    Test build 7

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,40 +1,24 @@
-import React from 'react';
+import React from "react";
+import { Route, Routes } from 'react-router-dom'; // Remove BrowserRouter import here
+import "./App.css";
+import Nav from "./components/Nav";
+import AllProducts from "./pages/AllProducts";
+import SingleProduct from "./components/SingleProduct";
 
 function App() {
   return (
-    <div>
-      <h1>Hello from React!</h1>
-      <p>This is a test page to confirm our React-Rails integration is working.</p>
+    <div className="app">
+      <Nav />
+      <div className="container">
+        <Routes>
+          <Route path="home" element={<AllProducts />} />
+          <Route path="/" element={<AllProducts />} /> 
+          <Route path="browse-products" element={<AllProducts />} /> 
+          <Route path="product/:id" element={<SingleProduct />} /> 
+        </Routes>
+      </div>
     </div>
   );
 }
 
 export default App;
-
-
-
-
-// import React from "react";
-// import { Route, Routes } from 'react-router-dom'; // Remove BrowserRouter import here
-// import "./App.css";
-// import Nav from "./components/Nav";
-// import AllProducts from "./pages/AllProducts";
-// import SingleProduct from "./components/SingleProduct";
-
-// function App() {
-//   return (
-//     <div className="app">
-//       <Nav />
-//       <div className="container">
-//         <Routes>
-//           <Route path="home" element={<AllProducts />} />
-//           <Route path="/" element={<AllProducts />} /> 
-//           <Route path="browse-products" element={<AllProducts />} /> 
-//           <Route path="product/:id" element={<SingleProduct />} /> 
-//         </Routes>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,29 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'; // Import the main App component
+import { createRoot } from 'react-dom/client'; 
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-// React 17 and earlier
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
-
-
-
-// import React from 'react';
-// import { createRoot } from 'react-dom/client'; 
-// import App from './App';
-// import { BrowserRouter } from 'react-router-dom';
-
-// const container = document.getElementById('root');
-// const root = createRoot(container);
-
-// root.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
