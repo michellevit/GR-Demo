c24331db44c4e433e868e85a97d69c70ed0885fe
michellevit <mvflandin@gmail.com>
2024-02-10 14:38:08 -0800

    Modify get request route on AllOrders

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
     def react_app
-        render file: Rails.root.join('public', 'build', 'index.html'), layout: false
+        render file: Rails.root.join('public', 'build', 'index.html')
     end
   end
