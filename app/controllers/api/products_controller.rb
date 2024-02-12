939b37baca3757c0bb6cafd3f9d8387c39b01d0e
michellevit <mvflandin@gmail.com>
2024-02-11 19:18:14 -0800

    Update backend formatting

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,7 +1,7 @@
 module Api
   class ProductsController < ApplicationController
     before_action :set_product, only: %i[ show edit update destroy ]
-    layout 'api'
+    layout 'application'
     
     # GET /products or /products.json
     def index
