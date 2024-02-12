44c2696ba3aad03bf8630a33af4db1879862969f
michellevit <mvflandin@gmail.com>
2024-02-11 20:09:50 -0800

    Update backend formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
     def react_app
-        render file: Rails.root.join('public', 'index.html')
+      render file: Rails.root.join('public', 'index.html'), layout: false, content_type: 'text/html'
     end
   end
