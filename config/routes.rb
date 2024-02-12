f93eef5dfbd4d9aa04c8ebe73162a0ebc1c75cd5
michellevit <mvflandin@gmail.com>
2024-02-11 16:04:23 -0800

    Update url for fetching products in frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
-    resources :products
-  end
+  resources :products
   root 'application#react_app'
   # Health check route
   get '/up', to: 'rails/health#show', as: :rails_health_check
