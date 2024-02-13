ff1f4560bab48c09820ebe30900b2a017e001845
michellevit <mvflandin@gmail.com>
2024-02-12 18:32:56 -0800

    Commit before resetting database tables

--- scripts/clear-tables.bat
+++ scripts/clear-tables.bat
@@ -19,7 +19,7 @@ IF %product_count% GTR 0 (
     echo Resetting primary key sequences...
     SET basePath=%cd%
     cd "%basePath%\scripts"
-    call reset-products-ids.bat
+    call reset-table-ids.bat
 ) ELSE (
     echo No entries found. Skipping deletion.
 )
