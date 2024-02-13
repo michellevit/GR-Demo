8b95f77ed42fadddd6c500f5055ad35d3c133357
michellevit <mvflandin@gmail.com>
2024-02-12 23:01:38 -0800

    Commit before resetting database tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_13_031809) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_13_070043) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_13_031809) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "products", "users"
