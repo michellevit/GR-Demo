559c44a7a1b8352946788d7955bc149c75c472c2
michellevit <mvflandin@gmail.com>
2024-02-13 21:33:18 -0800

    Adjust search route order

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   namespace :api do
+    # Define the search route before the resources block to ensure it's matched first
+    get 'products/search', to: 'products#search'
     resources :products do
       post 'like', on: :member
       get 'bundles', on: :member
     end
-    get 'products/search', to: 'products#search'
     resources :users do
       collection do
         get 'find_by_email', to: 'users#find_by_email' 
       end
     end
     resources :bundles
+    # Catchall must be last
     get '/', to: 'home#index', as: 'home'
   end
   root 'application#react_app'
