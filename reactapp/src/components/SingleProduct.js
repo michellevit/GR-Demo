d6a41fbddce2f0c91021657d98eadab82d45b827
michellevit <mvflandin@gmail.com>
2024-02-13 18:27:58 -0800

    Add import statement for ProductCard

--- reactapp/src/components/SingleProduct.js
+++ reactapp/src/components/SingleProduct.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./SingleProduct.css";
+import ProductCard from "./ProductCard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faStar,
