274deaa21fc9f8b0417fcf2870dd988745f94202
michellevit <mvflandin@gmail.com>
2024-02-14 19:08:08 -0800

    Convert file to typescript

--- reactapp/src/components/RatingsBarChart.tsx
+++ reactapp/src/components/RatingsBarChart.tsx
@@ -1,34 +1,35 @@
 import React from "react";
 import "./RatingsBarChart.css";
 
-const RatingsBarChart = ({ distribution }) => {
-  const totalRatings = Object.values(distribution).reduce(
-    (sum, value) => sum + value,
-    0
-  );
+interface RatingsBarChartProps {
+  distribution: Record<number, number>;
+}
+
+const RatingsBarChart: React.FC<RatingsBarChartProps> = ({ distribution }) => {
+  const totalRatings = Object.values(distribution).reduce((sum, value) => sum + value, 0);
 
   return (
     <div className="ratings-bar-chart-container">
-    <h3>Ratings</h3>
-    <div className="ratings-bar-chart">
-      {Object.entries(distribution).map(([stars, count]) => {
-        const widthPercentage = (count / totalRatings) * 100;
-        return (
-          <div className="bar-row" key={stars}>
-            <div className="stars">{stars} star</div>
-            <div className="outer-bar">
-              <div
-                className="inner-bar"
-                style={{
-                  width: `${widthPercentage}%`,
-                }}
-              ></div>
+      <h3>Ratings</h3>
+      <div className="ratings-bar-chart">
+        {Object.entries(distribution).map(([stars, count]) => {
+          const widthPercentage = (count / totalRatings) * 100;
+          return (
+            <div className="bar-row" key={stars}>
+              <div className="stars">{stars} star</div>
+              <div className="outer-bar">
+                <div
+                  className="inner-bar"
+                  style={{
+                    width: `${widthPercentage}%`,
+                  }}
+                ></div>
+              </div>
+              <div className="percentage">{Math.round(widthPercentage) + "%"}</div>
             </div>
-            <div className="percentage">{Math.round(widthPercentage) + "%"}</div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
